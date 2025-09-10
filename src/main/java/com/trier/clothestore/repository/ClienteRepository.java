0391105cd530db217968b5ea2496e0e7c23ef67c
KAROLINE MONTEIRO DE SOUZA <karoline_m_souza@estudante.sesisenai.org.br>
2025-09-10 16:32:25 -0300

    Alteração nome

--- src/main/java/com/trier/clothestore/repository/ClienteRepository.java
+++ src/main/java/com/trier/clothestore/repository/ClienteRepository.java
@@ -1,14 +1,13 @@
 package com.farmacia.elessandro.repository;
 
-import com.farmacia.elessandro.model.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
     @Repository
-    public interface ClienteRepository extends JpaRepository<ClienteModel, Integer> {
-        Optional<ClienteModel> findByCdCliente(Integer cdCliente);
-        Optional<ClienteModel> findAllByCdCliente(Integer cdCliente);
+    public interface ClienteRepository extends JpaRepository<com.farmacia.elessandro.model.UsuarioModel, Integer> {
+        Optional<com.farmacia.elessandro.model.UsuarioModel> findByCdCliente(Integer cdCliente);
+        Optional<com.farmacia.elessandro.model.UsuarioModel> findAllByCdCliente(Integer cdCliente);
 
     }
