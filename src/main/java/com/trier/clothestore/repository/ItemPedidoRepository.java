1f979dda0b59a79bf9490c9b19641c17a090bce1
elessandroabreu <binho_abreu@hotmail.com>
2025-09-11 15:26:05 -0300

    classes quase prontas

--- src/main/java/com/trier/clothestore/repository/ItemPedidoRepository.java
+++ src/main/java/com/trier/clothestore/repository/ItemPedidoRepository.java
@@ -1,5 +1,6 @@
 package com.trier.clothestore.repository;
 
+import com.trier.clothestore.model.ItemPedidoModel;
 import com.trier.clothestore.model.PedidoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ItemPedidoRepository extends JpaRepository<PedidoModel, Integer> {
-    Optional<PedidoModel> findByIdItemPedido(Integer idItemPedido);
-    Optional<PedidoModel> findAllByIdItemPedido(Integer idItemPedido);
+public interface ItemPedidoRepository extends JpaRepository<ItemPedidoModel, Integer> {
+    Optional<ItemPedidoModel> findByIdItemPedido(Integer idItemPedido);
+    Optional<ItemPedidoModel> findAllByIdItemPedido(Integer idItemPedido);
 
 }
